2d58f34893b00599fba5e3037da58a501a024c2b
athena272 <guilhermera272@gmail.com>
2024-12-27 23:46:57 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react"
 import Header from "@/components/Header";
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider >
   )
 }
