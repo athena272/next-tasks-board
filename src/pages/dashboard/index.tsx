2a82818737e6d89a3454fa0af521cf488d8f4328
athena272 <guilhermera272@gmail.com>
2024-12-28 21:07:01 -0300

    Update index.tsx

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,25 @@
+import { GetServerSideProps } from "next"
+import { getSession } from "next-auth/react"
 import Head from "next/head"
 import styles from './Dashboard.module.css'
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const session = await getSession({ req })
+    // console.log("🚀 ~ constgetServerSideProps:GetServerSideProps= ~ session:", session)
+    if (!session?.user) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
+}
+
 
 export default function Dashboard() {
     return (
