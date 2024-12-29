731b4c1016fad3b32f300b28ccc7e648816a9005
athena272 <guilhermera272@gmail.com>
2024-12-28 23:04:00 -0300

    Update index.tsx

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from "next"
 import { getSession } from "next-auth/react"
 import Head from "next/head"
 import styles from './Dashboard.module.css'
+import Textarea from "@/components/Textarea"
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     const session = await getSession({ req })
@@ -27,7 +28,22 @@ export default function Dashboard() {
                 <title>Meu painel de tarefas</title>
             </Head>
 
-            <h1>Pagina Painel</h1>
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        <h1 className={styles.title}>Qual sua tarefa?</h1>
+                        <form>
+                            <Textarea placeholder="Digite qual sua tarefa..." />
+                            <div className={styles.checkboxArea}>
+                                <input type="checkbox" className={styles.checkbox} />
+                                <label>Deixar tarefa publica?</label>
+                            </div>
+
+                            <button className={styles.button} type="submit">Registrar</button>
+                        </form>
+                    </div>
+                </section>
+            </main>
         </div>
     )
 }
